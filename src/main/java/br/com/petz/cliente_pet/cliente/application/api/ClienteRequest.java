ef9444c9c6390889d97be5af13c5f0bd8d85d672
allanbgd <allanbrenogomesdias@gmail.com>
2026-07-06 10:17:24 -0300

    implementa request e response

--- src/main/java/br/com/petz/cliente_pet/cliente/application/api/ClienteRequest.java
+++ src/main/java/br/com/petz/cliente_pet/cliente/application/api/ClienteRequest.java
@@ -1,4 +1,29 @@
 package br.com.petz.cliente_pet.cliente.application.api;
 
+import br.com.petz.cliente_pet.cliente.domain.Sexo;
+import lombok.Value;
+import org.hibernate.validator.constraints.br.CPF;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import java.time.LocalDate;
+
+@Value
 public class ClienteRequest {
+    @NotBlank
+    private String nomeCompleto;
+    @Email
+    @NotBlank
+    private String email;
+    @NotBlank
+    private String celular;
+    private String telefone;
+    private Sexo sexo;
+    @NotNull
+    private LocalDate dataNascimento;
+    @CPF
+    private String cpf;
+    @NotNull
+    private Boolean aceitaTermos;
 }
