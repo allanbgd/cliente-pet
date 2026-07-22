5b77ab46863867855a2372b5e1287ee2b64d6199
allanbgd <allanbrenogomesdias@gmail.com>
2026-07-22 11:36:54 -0300

    implementa pet domain

--- src/main/java/br/com/petz/cliente_pet/pet/domain/Pet.java
+++ src/main/java/br/com/petz/cliente_pet/pet/domain/Pet.java
@@ -1,4 +1,55 @@
 package br.com.petz.cliente_pet.pet.domain;
 
+import br.com.petz.cliente_pet.cliente.application.api.ClienteAlteracaoRequest;
+import br.com.petz.cliente_pet.cliente.application.api.ClienteRequest;
+import br.com.petz.cliente_pet.cliente.domain.Sexo;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.validator.constraints.br.CPF;
+
+import javax.persistence.*;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+@Getter
+@Entity
+//dados e comportamentos no mesmo lugar
 public class Pet {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(columnDefinition = "uuid",name = "idPet", updatable = false, unique = true, nullable = false)
+    private UUID idPet;
+    @NotBlank
+    private String nomePet;
+
+    @Enumerated(EnumType.STRING)
+    private Porte porte;
+
+    @Enumerated(EnumType.STRING)
+    private TipoPet tipo;
+
+    private String microchip;
+
+    private String raca;
+
+    @Enumerated(EnumType.STRING)
+    private SexoPet sexo;
+
+    private String pelagemCor;
+    @NotNull
+    private LocalDate dataNascimento;
+
+    private String rga;
+    private Integer peso;
+
+    private LocalDateTime dataHoraDoCadastro;
+    private LocalDateTime dataHoraDaUltimaAlteracao;
+
+
 }
