b6738fa8b1a076ee57ac156c7e9b03bd8bfcb4bf
allanbgd <allanbrenogomesdias@gmail.com>
2026-07-22 17:44:41 -0300

    implementa request e response

--- src/main/java/br/com/petz/cliente_pet/pet/domain/application/api/PetRequest.java
+++ src/main/java/br/com/petz/cliente_pet/pet/domain/application/api/PetRequest.java
@@ -1,4 +1,33 @@
 package br.com.petz.cliente_pet.pet.domain.application.api;
 
+import br.com.petz.cliente_pet.pet.domain.Porte;
+import br.com.petz.cliente_pet.pet.domain.SexoPet;
+import br.com.petz.cliente_pet.pet.domain.TipoPet;
+import lombok.Value;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+
+@Value
 public class PetRequest {
+    @NotBlank
+    private String nomePet;
+    private Porte porte;
+    @NotNull
+    private TipoPet tipo;
+    private String microchip;
+    @NotBlank
+    private String raca;
+    @NotNull
+    private SexoPet sexo;
+    private String pelagemCor;
+    @NotNull
+    private LocalDate dataNascimento;
+    private String rga;
+    private Integer peso;
 }
